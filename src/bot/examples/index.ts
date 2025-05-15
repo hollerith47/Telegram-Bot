81c7c6cfac1356c39ac2e9fde63a363bee744079
M Agung Dirgantara Azly Bagus Wibawa <agungmasda29@gmail.com>
2025-05-15 14:37:33 +0700

    feat(bot): clean up example files and update entry point

--- src/bot/examples/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as example1 } from './example-1';
-export { default as example2 } from './example-2';
-export { default as example3 } from './example-3';
-export { default as example4 } from './example-4';
